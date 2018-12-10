90219c7d12ecad9f9fe0c540f22ca10024b8a1c2
2219214078 <41150782+2219214078@users.noreply.github.com>
2018-12-10 16:00:27 +0800

    Add files via upload

---
+++ config/base.php
@@ -0,0 +1,105 @@
+<?php return array (
+  'site_name' => '小飞云盘',
+  'password' => 'yunfei521',
+  'style' => 'nexmoe',
+  'onedrive_root' => '/',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+      0 => '',
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'nkpwaBDOLD09[|qzZI172*$',
+  'client_id' => 'debb9175-12a0-4ff4-af28-fe2e85c3bf68',
+  'redirect_uri' => 'https://ju.tn/',
+  'refresh_token' => 'OAQABAAAAAAC5una0EUFgTIF8ElaxtWjT94IlF8mxKiAmRIfmicDV9pc5R7LuA5AYL9aQrR_epQ3CakTBN_UvhEsBtXWYUdnHd-q_2kVvqD8OUTJzEmymwsBDlghx3gtPxAZ6D4MJOwno6Rs9Kt7g8nSE2oFYAA9qiv8ZWSW9fSffcpfPcuwH3EfLxAvIa5_IKbGlyMqBSJnQaOcoPuqxl2Jx-jrosuKjVcKkIWaZ42Wz4xqINcZWN54bJoa9qbZ1fE-2yPjjM5DXPeIKcq2qEHBf-aflQk4kHaPxzXxERb9DGMflb_St9GuvuxBNXpDPX-ETFQvl9ZYZvRrtJZvJZS1mjNHWgwnI4f6cB0qNmEFmZ24Fsa8LbevNWeMB1L1Z-PmXb9WVRtf1h0aJ5z3Umpc1u4dwzo6AcmWd4xNKTl1SsWezsgMOo5uL14UnuUBxRKZqM0zLhM4n3HFReXL9wFzVT2RFu7s74L1YvCq2mzoWkyYceT7_9bRQpdaN81I-scGJQJcNwPPozeUeT4haOLiaxZ7GS9OU8xTcTDY7VfbcpZWZ6wWTgprir-V6z95KPJ67r8VkyMcGA1EajPA_pS4ILjupE5oR_D8_dkdFjUb_yvEj-btmTMfXvA1oQm73dON62kgVSgbKxL6PAAgy46jvXCHUbi3hA9ltHz7cYG1zgxlwTboh89pupRJDRIK3RB7xPgT_7jNtkMYhzJaGmNzjKyQ6TGNYi4hTewgRu70OuDkwA3VtbtmC-AWKvTy8jI2wEMUioPA2kjzWAwF64oZZpKW2iaqsX5Wek4dBUgjnc51aL8rA8hXRIjpAlaY4cvEGhprFHGEgAA',
+);
---
+++ config/upload.php
@@ -0,0 +1,482 @@
+<?php return array (
+  '/upload/lib/view.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/lib/view.php',
+    'remotepath' => '/upload/lib/view.php',
+    'filesize' => 2277,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/one.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/one.php',
+    'remotepath' => '/upload/one.php',
+    'filesize' => 6168,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/php.ini' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/php.ini',
+    'remotepath' => '/upload/php.ini',
+    'filesize' => 20,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/admin/cache.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/admin/cache.php',
+    'remotepath' => '/upload/view/admin/cache.php',
+    'filesize' => 2071,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/admin/images.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/admin/images.php',
+    'remotepath' => '/upload/view/admin/images.php',
+    'filesize' => 1299,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/admin/install/install_0.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/admin/install/install_0.php',
+    'remotepath' => '/upload/view/admin/install/install_0.php',
+    'filesize' => 2264,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/admin/install/install_1.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/admin/install/install_1.php',
+    'remotepath' => '/upload/view/admin/install/install_1.php',
+    'filesize' => 2348,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/admin/install/install_2.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/admin/install/install_2.php',
+    'remotepath' => '/upload/view/admin/install/install_2.php',
+    'filesize' => 524,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/admin/install/install_3.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/admin/install/install_3.php',
+    'remotepath' => '/upload/view/admin/install/install_3.php',
+    'filesize' => 847,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/admin/install/layout.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/admin/install/layout.php',
+    'remotepath' => '/upload/view/admin/install/layout.php',
+    'filesize' => 2047,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/admin/layout.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/admin/layout.php',
+    'remotepath' => '/upload/view/admin/layout.php',
+    'filesize' => 3154,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/admin/login.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/admin/login.php',
+    'remotepath' => '/upload/view/admin/login.php',
+    'filesize' => 781,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/admin/setpass.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/admin/setpass.php',
+    'remotepath' => '/upload/view/admin/setpass.php',
+    'filesize' => 979,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/admin/settings.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/admin/settings.php',
+    'remotepath' => '/upload/view/admin/settings.php',
+    'filesize' => 2529,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/admin/show.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/admin/show.php',
+    'remotepath' => '/upload/view/admin/show.php',
+    'filesize' => 752,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/admin/upload.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/admin/upload.php',
+    'remotepath' => '/upload/view/admin/upload.php',
+    'filesize' => 3622,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/classic/404.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/classic/404.php',
+    'remotepath' => '/upload/view/classic/404.php',
+    'filesize' => 3116,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/classic/layout.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/classic/layout.php',
+    'remotepath' => '/upload/view/classic/layout.php',
+    'filesize' => 1595,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/classic/list.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/classic/list.php',
+    'remotepath' => '/upload/view/classic/list.php',
+    'filesize' => 4541,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/material/404.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/material/404.php',
+    'remotepath' => '/upload/view/material/404.php',
+    'filesize' => 184,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/material/images/index.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/material/images/index.php',
+    'remotepath' => '/upload/view/material/images/index.php',
+    'filesize' => 602,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/material/images/layout.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/material/images/layout.php',
+    'remotepath' => '/upload/view/material/images/layout.php',
+    'filesize' => 2087,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/material/layout.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/material/layout.php',
+    'remotepath' => '/upload/view/material/layout.php',
+    'filesize' => 2265,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/material/list.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/material/list.php',
+    'remotepath' => '/upload/view/material/list.php',
+    'filesize' => 5340,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/material/password.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/material/password.php',
+    'remotepath' => '/upload/view/material/password.php',
+    'filesize' => 785,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/material/show/audio.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/material/show/audio.php',
+    'remotepath' => '/upload/view/material/show/audio.php',
+    'filesize' => 1298,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/material/show/code.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/material/show/code.php',
+    'remotepath' => '/upload/view/material/show/code.php',
+    'filesize' => 1626,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/material/show/doc.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/material/show/doc.php',
+    'remotepath' => '/upload/view/material/show/doc.php',
+    'filesize' => 290,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/material/show/image.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/material/show/image.php',
+    'remotepath' => '/upload/view/material/show/image.php',
+    'filesize' => 954,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/material/show/pdf.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/material/show/pdf.php',
+    'remotepath' => '/upload/view/material/show/pdf.php',
+    'filesize' => 1193,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/material/show/stream.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/material/show/stream.php',
+    'remotepath' => '/upload/view/material/show/stream.php',
+    'filesize' => 2941,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/material/show/video.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/material/show/video.php',
+    'remotepath' => '/upload/view/material/show/video.php',
+    'filesize' => 1455,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/material/show/video2.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/material/show/video2.php',
+    'remotepath' => '/upload/view/material/show/video2.php',
+    'filesize' => 1571,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/material/show/video5.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/material/show/video5.php',
+    'remotepath' => '/upload/view/material/show/video5.php',
+    'filesize' => 956,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/nexmoe/404.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/nexmoe/404.php',
+    'remotepath' => '/upload/view/nexmoe/404.php',
+    'filesize' => 184,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/nexmoe/images/index.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/nexmoe/images/index.php',
+    'remotepath' => '/upload/view/nexmoe/images/index.php',
+    'filesize' => 651,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/nexmoe/images/layout.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/nexmoe/images/layout.php',
+    'remotepath' => '/upload/view/nexmoe/images/layout.php',
+    'filesize' => 1176,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/nexmoe/layout.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/nexmoe/layout.php',
+    'remotepath' => '/upload/view/nexmoe/layout.php',
+    'filesize' => 2577,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/nexmoe/list.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/nexmoe/list.php',
+    'remotepath' => '/upload/view/nexmoe/list.php',
+    'filesize' => 7084,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/nexmoe/password.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/nexmoe/password.php',
+    'remotepath' => '/upload/view/nexmoe/password.php',
+    'filesize' => 785,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/nexmoe/show/audio.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/nexmoe/show/audio.php',
+    'remotepath' => '/upload/view/nexmoe/show/audio.php',
+    'filesize' => 904,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/nexmoe/show/code.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/nexmoe/show/code.php',
+    'remotepath' => '/upload/view/nexmoe/show/code.php',
+    'filesize' => 1720,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/nexmoe/show/doc.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/nexmoe/show/doc.php',
+    'remotepath' => '/upload/view/nexmoe/show/doc.php',
+    'filesize' => 134,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/nexmoe/show/image.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/nexmoe/show/image.php',
+    'remotepath' => '/upload/view/nexmoe/show/image.php',
+    'filesize' => 995,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/nexmoe/show/pdf.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/nexmoe/show/pdf.php',
+    'remotepath' => '/upload/view/nexmoe/show/pdf.php',
+    'filesize' => 1193,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/nexmoe/show/stream.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/nexmoe/show/stream.php',
+    'remotepath' => '/upload/view/nexmoe/show/stream.php',
+    'filesize' => 2901,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/nexmoe/show/video.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/nexmoe/show/video.php',
+    'remotepath' => '/upload/view/nexmoe/show/video.php',
+    'filesize' => 1265,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/.dockerignore' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/.dockerignore',
+    'remotepath' => '/upload/.dockerignore',
+    'filesize' => 15,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/.htaccess' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/.htaccess',
+    'remotepath' => '/upload/.htaccess',
+    'filesize' => 127,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/Dockerfile' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/Dockerfile',
+    'remotepath' => '/upload/Dockerfile',
+    'filesize' => 481,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/README.md' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/README.md',
+    'remotepath' => '/upload/README.md',
+    'filesize' => 3097,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/cache/README.md' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/cache/README.md',
+    'remotepath' => '/upload/cache/README.md',
+    'filesize' => 36,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/cache/cachedata.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/cache/cachedata.php',
+    'remotepath' => '/upload/cache/cachedata.php',
+    'filesize' => 105120508,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/config/README.md' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/config/README.md',
+    'remotepath' => '/upload/config/README.md',
+    'filesize' => 36,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/config/base.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/config/base.php',
+    'remotepath' => '/upload/config/base.php',
+    'filesize' => 2703,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/config/token.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/config/token.php',
+    'remotepath' => '/upload/config/token.php',
+    'filesize' => 2913,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/config/upload.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/config/upload.php',
+    'remotepath' => '/upload/config/upload.php',
+    'filesize' => 19281,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/config/uploaded.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/config/uploaded.php',
+    'remotepath' => '/upload/config/uploaded.php',
+    'filesize' => 1871,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/controller/AdminController.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/controller/AdminController.php',
+    'remotepath' => '/upload/controller/AdminController.php',
+    'filesize' => 5701,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+  '/upload/view/nexmoe/show/video5.php' => 
+  array (
+    'localfile' => '/home/ftp/s/s4240377/wwwroot/view/nexmoe/show/video5.php',
+    'remotepath' => '/upload/view/nexmoe/show/video5.php',
+    'filesize' => 979,
+    'upload_type' => 'web',
+    'update_time' => 0,
+  ),
+);
